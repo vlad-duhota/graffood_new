de319921cfff8663b83931ee10461ee0b8b4251c
vlad-duhota <vladarduino@gmail.com>
2022-07-20 15:02:44 +0300

    form 4

--- js/main.js
+++ js/main.js
@@ -102,19 +102,19 @@ selectItem.forEach(elem => {
     })
 });
 
-$("form").submit(function() { // Событие отправки с формы
-    var form_data = $(this).serialize(); // Собираем данные из полей
-    jQuery.ajax({
-        type: "POST", // Метод отправки
-        url: "../sendform.php", // Путь к PHP обработчику sendform.php
-        data: form_data,
-        success: swal({
-            title: "Спасибо за заявку!",
-            type: "success",
-            showConfirmButton: false,
-            timer: 2000
-        })
-    });
-    $(this).find('input, textarea').prop('disabled', true);
-    event.preventDefault();
-});
+// $("form").submit(function() { // Событие отправки с формы
+//     var form_data = $(this).serialize(); // Собираем данные из полей
+//     jQuery.ajax({
+//         type: "POST", // Метод отправки
+//         url: "../sendform.php", // Путь к PHP обработчику sendform.php
+//         data: form_data,
+//         success: swal({
+//             title: "Спасибо за заявку!",
+//             type: "success",
+//             showConfirmButton: false,
+//             timer: 2000
+//         })
+//     });
+//     $(this).find('input, textarea').prop('disabled', true);
+//     event.preventDefault();
+// });
